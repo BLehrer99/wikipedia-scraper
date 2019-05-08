e296973a65a938884ce02873f8f51ed11a335040
BLehrer <Benjaminrlehrer@gmail.com>
2019-05-08 11:25:24 -0400

    add exceptions

--- scraper.py
+++ scraper.py
@@ -1,7 +1,15 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
+from urllib.error import URLError
+
 from bs4 import BeautifulSoup
 
-html = urlopen('http://www.pythonscraping.com/pages/page1.html')
-
-bs = BeautifulSoup(html.read(), 'html.parser')
-print(bs.h1)
+try:
+    html = urlopen('https://en.wikipedia.org/wiki/Philosophy')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print('The server could not be found!')
+else:
+    bs = BeautifulSoup(html.read(), 'html.parser')
+    print(bs.html.body.h1)
